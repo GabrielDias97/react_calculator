baac4b65578c1ea6e64ddad96d8f1c6e81ac0cb0
GabrielDias97 <Gabrieldias97@hotmail.com>
2023-11-07 13:20:30 -0300

    Keyboard layout

--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -1,12 +1,24 @@
 import styles from './Calculator.module.css'
 
 const Calculator = () => {
+
+  const keypadNumbers = ['1', '2', '3', '4', '5', '6', '7', '8', '9',];
+  const operations = ['+', '-', '*', '/']
+
   return (
     <div className={styles.calculator}>
-        <div className={styles.completeOperation}>3+3=6</div>
-        <div className={styles.display}>6</div>
-        <div className={styles.buttons}></div>
-        <button>=</button>
+        <div className={styles.completeOperation}></div>
+        <div className={styles.display}></div>
+        <div className={styles.buttons}>
+          <button>AC</button>
+          {keypadNumbers.map((num) => (
+            <button key={num}>{num}</button>
+          ))}
+          {operations.map((operation) => (
+            <button key={operation}>{operation}</button>
+          ))}
+          <button>=</button>
+        </div>
     </div>
   )
 }
